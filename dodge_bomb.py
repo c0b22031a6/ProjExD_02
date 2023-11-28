6e28be175ff3f8bd2cf4cbb5f70e9c6e563d67ca
c0b22031 <c0b22031a6@edu.teu.ac.jp>
2023-11-28 16:53:10 +0900

    追加機能3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,9 +3,9 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1000, 600
+WIDTH, HEIGHT = 1000, 550
 
-delta = {
+delta = {  #練習3:押下キーと移動量の辞書 
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
@@ -14,16 +14,17 @@ delta = {
 }
 
 
+
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
     オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
-    引数rct：こうかとんor爆弾SurfaceのRect
-    戻り値：横方向，縦方向はみ出し判定結果（画面内：True／画面外：False）
+    引数rct:こうかとんor爆弾SurfaceのRect
+    戻り値:横方向,縦方向はみ出し判定結果(画面内:True/画面外:False)
     """
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向はみ出し判定
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向はみ出し判定
         tate = False
     return yoko, tate
     
@@ -33,26 +34,37 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = 900, 400
-    bb_img = pg.Surface((20,20))
-    bb_img.set_colorkey((0, 0, 0))
-    pg.draw.circle(bb_img, (255, 0, 0),(10, 10), 10)
-    bb_rct = bb_img.get_rect()
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0) 
+    kk_rct = kk_img.get_rect()  # 練習3:こうかとんのRectを抽出する
+    kk_rct.center = 800, 400  # 練習3:こうかとんの初期座標
+    bb_img = pg.Surface((20,20))  # 練習1:透明のSurface
+    bb_img.set_colorkey((0, 0, 0))  # 練習1:黒い部分を透明
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習１：赤い半径10の円を描く  
+    bb_rct = bb_img.get_rect()  # 練習1:爆弾のRectを抽出する
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
-    vx, vy = +5, +5
+    vx, vy = +5, +5  # 練習2:爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
+    num1 = 100
+    
+    
+    
+
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
             
         if kk_rct.colliderect(bb_rct):
+            num1 = 0
+        num1 += 1
+        if num1 == 2:
             print("Game Over")
-            return
+            return 
+        
         
         
         key_lst = pg.key.get_pressed()
@@ -61,6 +73,26 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
+
+        """""
+        if sum_mv == [-5,0]:
+            kk_img = pg.transform.rotozoom(kk_img, 0, 1.0) 
+        if sum_mv == [-5,-5]:
+            kk_img1 = pg.transform.rotozoom(kk_img, 315, 1.0)
+        if sum_mv == [0,-5]:
+            kk_img2 = pg.transform.rotozoom(kk_img, 270, 1.0)
+        if sum_mv == [+5,-5]:
+            kk_img3 = pg.transform.rotozoom(kk_img, 225, 1.0)
+        if sum_mv == [+5,0]:
+            kk_img4 = pg.transform.rotozoom(kk_img, 180, 1.0)
+        if sum_mv == [+5,+5]:
+            kk_img5 = pg.transform.rotozoom(kk_img, 135, 1.0)
+        if sum_mv == [0,+5]:
+            kk_img6 = pg.transform.rotozoom(kk_img, 90, 1.0)
+        if sum_mv == [-5,+5]:
+            kk_img7 = pg.transform.rotozoom(kk_img, 45, 1.0)
+        """
+        
 
 
         screen.blit(bg_img, [0, 0])
@@ -79,6 +111,10 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
+        if num1 < 2:
+            kk_img = pg.image.load("ex02/fig/6.png")
+            kk_img = pg.transform.rotozoom(kk_img,0,2.0)
 
 
 if __name__ == "__main__":
