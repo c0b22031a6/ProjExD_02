8b9beda798e5aa347cbf42b16bbcab28e0ff61d4
c0b22031 <c0b22031a6@edu.teu.ac.jp>
2023-11-28 17:55:46 +0900

    コードをまとめる　#2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,22 +5,23 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1000, 550
 
+
 delta = {  #練習3:押下キーと移動量の辞書 
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0)
-
 }
 
 
-
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    
     """
     オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
     引数rct:こうかとんor爆弾SurfaceのRect
     戻り値:横方向,縦方向はみ出し判定結果(画面内:True/画面外:False)
     """
+
     yoko, tate = True, True
     if rct.left < 0 or WIDTH < rct.right:  # 横方向はみ出し判定
         yoko = False
@@ -46,26 +47,23 @@ def main():
     vx, vy = +5, +5  # 練習2:爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
-    num1 = 100
+    num1 = 100  # 追加機能3を実装するために設定
     
-    
-    
-
-
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
             
         if kk_rct.colliderect(bb_rct):
-            num1 = 0
-        num1 += 1
+            num1 = 0  #こうかとんが爆弾にぶつかったときにnum1を0にする
+        num1 += 1  # num1を１ずつプラスする
         if num1 == 2:
             print("Game Over")
-            return 
-        
-        
+            return
+
+        if num1 < 2:  # num1が２未満まで画像を切り替える
+            kk_img = pg.image.load("ex02/fig/6.png")  # 画像切り替え
+            kk_img = pg.transform.rotozoom(kk_img,0,2.0) 
         
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -74,8 +72,8 @@ def main():
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
 
-        """""
-        if sum_mv == [-5,0]:
+        """""  # 追加機能１の途中
+        if sum_mv == [-5,0]:  # 各移動量の合計タプルの時の画像をif文で挿入しようとした
             kk_img = pg.transform.rotozoom(kk_img, 0, 1.0) 
         if sum_mv == [-5,-5]:
             kk_img1 = pg.transform.rotozoom(kk_img, 315, 1.0)
@@ -84,7 +82,7 @@ def main():
         if sum_mv == [+5,-5]:
             kk_img3 = pg.transform.rotozoom(kk_img, 225, 1.0)
         if sum_mv == [+5,0]:
-            kk_img4 = pg.transform.rotozoom(kk_img, 180, 1.0)
+            kk_imgs = pg.transform.flip(kk_img, True, True)
         if sum_mv == [+5,+5]:
             kk_img5 = pg.transform.rotozoom(kk_img, 135, 1.0)
         if sum_mv == [0,+5]:
@@ -93,7 +91,6 @@ def main():
             kk_img7 = pg.transform.rotozoom(kk_img, 45, 1.0)
         """
         
-
 
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
@@ -110,11 +107,7 @@ def main():
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(50)
-
-        if num1 < 2:
-            kk_img = pg.image.load("ex02/fig/6.png")
-            kk_img = pg.transform.rotozoom(kk_img,0,2.0)
+        clock.tick(50)      
 
 
 if __name__ == "__main__":
